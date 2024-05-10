2bdc6974dd557b9b9728e50e428241675ed4b552
Classmaterma <classmaterma@gmail.com>
2024-05-10 16:18:38 +0900

    add plugin, design files

--- php/survey_widget.php
+++ php/survey_widget.php
@@ -41,7 +41,7 @@ while($row=$answers->fetch_assoc()){
                		<?php if(!isset($ans[$row['id']])): ?>
                			<a href="index.php?page=answer_survey&id=<?php echo $row['id'] ?>" class="btn btn-sm bg-gradient-primary"><i class="fa fa-pen-square"></i> Take Survey</a>
                		<?php else: ?>
-               			<p class="text-primary border-top border-primary">Done</p>
+               			<p class="text-primary border-top border-primary">완료</p>
                		<?php endif; ?>
                	</div>
                </div>
